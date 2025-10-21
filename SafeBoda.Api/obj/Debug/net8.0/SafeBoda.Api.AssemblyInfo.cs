6a6c97a486893da8b85eec48e42981c91a43dd3d
aline-001 <148322512+aline-001@users.noreply.github.com>
2025-10-21 21:08:57 +0200

    Safeboda application

--- SafeBoda.Api/obj/Debug/net8.0/SafeBoda.Api.AssemblyInfo.cs
+++ SafeBoda.Api/obj/Debug/net8.0/SafeBoda.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SafeBoda.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f163d3ed29047624d300dc5d317d84319b90fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580f3b6024534b151b4a906b566a4104085fc76f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SafeBoda.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SafeBoda.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
